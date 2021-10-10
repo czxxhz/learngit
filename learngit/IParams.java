005f50212ed2a68ec9567b19a58c65556c251713
czx <1433592445@qq.com>
2021-10-10 21:51:38 +0800

    commit all

---
+++ learngit/IParams.java
@@ -0,0 +1,14 @@
+package Í£³µÏµÍ³;
+import java.util.ArrayList;
+
+public interface IParams {
+
+    int getBigPSpace();
+
+    int getMediumPSpace();
+
+    int getSmallPSpace();
+
+    ArrayList<Integer> getParkingPlan();
+
+}
